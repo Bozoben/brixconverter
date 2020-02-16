b9475eedc52e7d43e1a70c0e971353740c8b539a
Benoît VILLETARD <benoit.villetard@maif.fr>
2020-02-16 13:47:05 +0100

    Initialisation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleChange(e) {
+    const degres = parseFloat(e.target.value);
+    if (!isNaN(degres)) {
+      const densite = 1 + degres * 4 / 1000;
+      document.getElementById("densite").innerHTML = '' + densite;
+    }
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h2>Conversion Brix</h2>
       </header>
+      <div className="App-body">
+      <br/>
+        <label for="brix" className="App-label"> Degrés Brix : </label>
+        <input type="text" name="brix" placeholder="Valeur en degré brix" onChange={handleChange}></input>
+        <br/><br/>
+        <div><label className="App-label">Densité : </label>
+        <span id="densite"></span></div>
+        </div>
     </div>
   );
 }
